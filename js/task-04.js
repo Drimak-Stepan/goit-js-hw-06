8ddf05db8612486854d901531ca5bede3d51c945
Stepan Drimak <108022018+Drimak-Stepan@users.noreply.github.com>
2022-11-03 20:14:57 +0200

    add var2 to t4

--- js/task-04.js
+++ js/task-04.js
@@ -3,19 +3,48 @@
 // Оновлюй інтерфейс новим значенням змінної counterValue.
 
 
-const counterValue = document.querySelector('#value');
-const decrementBtn = document.querySelector('[data-action="decrement"]');
-const incrementBtn = document.querySelector('[data-action="increment"]');
+const refs = {
+  value: document.querySelector('#value'),
+  decrement: document.querySelector('[data-action="decrement"]'),
+  increment: document.querySelector('[data-action="increment"]'),
+};
+let counterValue = 0 ;
 
-counterValue.textContent = 0;
-
-const decrement = function () {
-  counterValue.textContent -= decrementBtn.textContent * -1;
+const render =()=>{
+  refs.value.textContent = counterValue;
 };
 
-const increment = function () {
-  counterValue.textContent -= incrementBtn.textContent * -1;
+const handleDecrement = () =>{
+  counterValue -= 1;
+  render();
 };
 
-decrementBtn.addEventListener('click', decrement);
-incrementBtn.addEventListener('click', increment);
+const handleIncrement = () =>{
+  counterValue += 1;
+  render();
+};
+
+render();
+
+refs.decrement.addEventListener('click', handleDecrement);
+refs.increment.addEventListener('click', handleIncrement);
+
+
+
+//-----------var/2-----
+// const counterValue = document.querySelector('#value');
+// const decrementBtn = document.querySelector('[data-action="decrement"]');
+// const incrementBtn = document.querySelector('[data-action="increment"]');
+
+// counterValue.textContent = 0;
+
+// const decrement = function () {
+//   counterValue.textContent -= decrementBtn.textContent * -1;
+// };
+
+// const increment = function () {
+//   counterValue.textContent -= incrementBtn.textContent * -1;
+// };
+
+// decrementBtn.addEventListener('click', decrement);
+// incrementBtn.addEventListener('click', increment);
