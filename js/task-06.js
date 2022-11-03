92238529a5d56e44863f452fc20158928e1c4f29
Stepan Drimak <108022018+Drimak-Stepan@users.noreply.github.com>
2022-11-03 22:08:46 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,13 +1,16 @@
-// Напиши скрипт, який під час втрати фокусу на інпуті (подія blur), перевіряє його вміст щодо правильної кількості введених символів.
+// Напиши скрипт, який під час втрати фокусу на інпуті (подія blur), 
+//перевіряє його вміст щодо правильної кількості введених символів.
 // Яка кількість смиволів повинна бути в інпуті, зазначається в його атрибуті data-length.
-// Якщо введена правильна кількість символів, то border інпуту стає зеленим, якщо неправильна кількість - червоним.
-// Для додавання стилів використовуй CSS-класи valid і invalid, які ми вже додали у вихідні файли завдання.
+// Якщо введена правильна кількість символів, то border інпуту стає зеленим, 
+//якщо неправильна кількість - червоним.
+// Для додавання стилів використовуй CSS-класи valid і invalid, 
+//які ми вже додали у вихідні файли завдання.
 
 const passwordInput = document.querySelector('#validation-input');
 const passwordIndex = document.querySelector('#validation-input[data-length]');
 
 function passwordCheck() {
-if (passwordInput.value.length === Number(passwordInput.dataset.length)) {
+if (passwordInput.value.length === Number(passwordIndex.dataset.length)) {
     passwordInput.classList.add('valid');
     passwordInput.classList.remove('invalid');
 } else if (passwordInput.value.length > 0) {
