673cf2b195e99de5d84fccf93d84178f007301f3
Stepan Drimak <108022018+Drimak-Stepan@users.noreply.github.com>
2022-11-05 07:19:16 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -6,12 +6,13 @@ const colorBtn = document.querySelector('.change-color');
 const colorText = document.querySelector('.color');
 const body = document.querySelector('body');
 
-colorBtn.addEventListener('click', changeColor);
+colorBtn.addEventListener('click', getRandomHexColor);//()changeColor);
 
-function changeColor() {
+
+//function changeColor() {
   function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-  }
+  //}
   const randomColor = getRandomHexColor();
   body.style.backgroundColor = randomColor;
   colorText.textContent = randomColor;
