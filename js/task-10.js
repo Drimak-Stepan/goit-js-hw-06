fa37b3876b9be9c15d6539b9f8649fbbc56f351e
Stepan Drimak <108022018+Drimak-Stepan@users.noreply.github.com>
2022-11-10 11:18:44 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -6,6 +6,30 @@
 // Створи функцію destroyBoxes(), яка очищає вміст div#boxes, у такий спосіб видаляючи всі створені елементи.
 
 
+const number = document.querySelector('#controls > input');
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const innerBox = document.querySelector('#boxes');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const createBoxes = () => {
+  innerBox.innerHTML = '';
+  for (let i = 1; i <= number.value; i += 1) {
+    const box = document.createElement('div');
+    box.style.width = 20 + i * 10 + 'px';
+    box.style.height = 20 + i * 10 + 'px';
+    box.style.backgroundColor = getRandomHexColor();
+    innerBox.append(box);
+  }
+};
+
+const destroyBoxes = () => {
+  innerBox.innerHTML = '';
+  number.value = '';
+};
+
+createBtn.addEventListener('click', createBoxes);
+destroyBtn.addEventListener('click', destroyBoxes);
